810ff54b3f7ea112f7b443a3f3fae4c225e95a75
Grzegorz Kita <grzegorz.kita@protonmail.com>
2023-04-21 17:42:00 +0200

    Changed views to use view models as DataContext

--- MovieRental/View/Modals/EditMovieWindow.xaml.cs
+++ MovieRental/View/Modals/EditMovieWindow.xaml.cs
@@ -1,12 +1,9 @@
 using MovieRental.Model;
-using System;
 using System.Windows;
 
 namespace MovieRental.View.Modals;
 
 public partial class EditMovieWindow {
-
-    private MovieModel? _movieModel;
 
     public EditMovieWindow(MovieModel? movie = null) {
         InitializeComponent();
@@ -15,50 +12,11 @@ public partial class EditMovieWindow {
             Title = "Edit movie";
             btnDelete.Visibility = Visibility.Visible;
 
-            _movieModel = movie;
-
-            txtId.Text = movie.Id.ToString();
-            txtTitle.Text = movie.Title;
-            txtYear.Text = movie.Year;
-            txtGenre.Text = movie.Genre;
-            txtPlot.Text = movie.Plot;
-            txtCopies.Text = movie.Copies.ToString();
+            ViewModel.MovieModel = new MovieModel(movie);
         } else {
             Title = "Add movie";
         }
-    }
 
-    private void btnSave_Click(object sender, RoutedEventArgs e) {
-        try {
-            if (_movieModel != null) {
-                // TODO: Update model in database
-            } else {
-                MovieModel newMovie = new MovieModel(
-                    Convert.ToInt32(txtId.Text),
-                    txtTitle.Text,
-                    txtYear.Text,
-                    txtGenre.Text,
-                    txtPlot.Text,
-                    Convert.ToInt32(txtCopies.Text)
-                );
-
-                // TODO: Insert model to database
-            }
-
-            Close();
-        } catch (Exception exception) {
-            Console.WriteLine($"Error updating database:\n{exception}");
-            MessageBox.Show(this, exception.Message);
-        }
-    }
-
-    private void btnCancel_Click(object sender, RoutedEventArgs e) {
-        Close();
-    }
-
-    private void btnDelete_Click(object sender, RoutedEventArgs e) {
-        if (_movieModel == null) {
-            // TODO Handle deleting the model
-        }
+        ViewModel.UpdateMovieCompleted += Close;
     }
 }
