d2393abe691d40d301578dc3ae018c15e53142c5
Grzegorz Kita <grzegorz.kita@protonmail.com>
2023-05-14 20:32:20 +0200

    Override ToolTipService.ShowOnDisabledProperty to true

--- MovieRental/App.xaml.cs
+++ MovieRental/App.xaml.cs
@@ -1,6 +1,17 @@
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Threading;
 
 namespace MovieRental;
 
 public partial class App : Application {
+
+    protected override void OnStartup(StartupEventArgs e) {
+        base.OnStartup(e);
+
+        // https://stackoverflow.com/a/39348804
+        ToolTipService.ShowOnDisabledProperty.OverrideMetadata(
+            typeof(FrameworkElement),
+            new FrameworkPropertyMetadata(true));
+    }
 }
