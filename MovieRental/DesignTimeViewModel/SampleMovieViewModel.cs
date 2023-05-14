666aa5dbe8766dd5bf5754fb42da52fec798747c
Grzegorz Kita <grzegorz.kita@protonmail.com>
2023-05-14 21:17:03 +0200

    Improved SampleMovieCollectionViewModel

--- MovieRental/DesignTimeViewModel/SampleMovieViewModel.cs
+++ MovieRental/DesignTimeViewModel/SampleMovieViewModel.cs
@@ -5,7 +5,7 @@ namespace MovieRental.DesignTimeViewModel;
 
 public class SampleMovieViewModel : MovieViewModel {
 
-    public SampleMovieViewModel() : base(new Movie(1, "The Shawshank Redemption", "1994", "crime, drama", "", 3)) {}
+    public SampleMovieViewModel() : base(new Model.Movie(1,"The Shawshank Redemption","1994","crime, drama","Over the course of several years, two convicts form a friendship, seeking consolation and, eventually, redemption through basic compassion.",3)) {}
 
     public SampleMovieViewModel(Movie movieModel) : base(movieModel) {}
 }
